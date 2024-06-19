6f552e43cb7068dbfdca21638a71bb85cbed7daa
unknown <sayedsinan7@gmail.com>
2024-06-19 16:05:51 -0700

    modules downloaded and server created

--- build/index.js
+++ build/index.js
@@ -1,2 +1,29 @@
 "use strict";
-console.log('hello world');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = (0, express_1.default)();
+const server = http_1.default.createServer(app);
+// Express configuration
+app.use((0, cors_1.default)());
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.set("PORT", 3000);
+app.set("BASE_URL", "localhost");
+// Starting the server
+try {
+    const port = app.get("PORT");
+    const baseUrl = app.get("BASE_URL");
+    server.listen(port, () => {
+        console.log("The server is listening ");
+    });
+}
+catch (e) {
+    console.log(e);
+}
+exports.default = server;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('hello world')
+import express, {Express}from "express";
+import http  from "http";
+import cors from "cors"
+import bodyParser from "body-parser";
+import exp from "constants";
+const app:Express =express();
+const server=http.createServer(app);
+
+// Express configuration
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.set("PORT",3000)
+app.set("BASE_URL","localhost")
+// Starting the server
+try{
+    const port =app.get("PORT")
+    const baseUrl =app.get("BASE_URL")
+
+    server.listen(port,(): void =>{
+        console.log("The server is listening ")
+    })
+}catch(e){
+    console.log(e)
+}
+export default server
